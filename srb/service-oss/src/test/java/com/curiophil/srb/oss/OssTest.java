3d8ffaf8580af854ca2cacf07a63ff281c0ecb52
curiophil <curiosityphil@gmail.com>
2022-07-15 15:12:23 +0800

    去掉aliyun accesskey1

--- srb/service-oss/src/test/java/com/curiophil/srb/oss/OssTest.java
+++ srb/service-oss/src/test/java/com/curiophil/srb/oss/OssTest.java
@@ -17,12 +17,12 @@ public class OssTest {
     @Test
     public void testUpload() throws IOException {
         // yourEndpoint填写Bucket所在地域对应的Endpoint。以华东1（杭州）为例，Endpoint填写为https://oss-cn-hangzhou.aliyuncs.com。
-        String endpoint = "oss-cn-nanjing.aliyuncs.com";
+        String endpoint = "****";
 // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        String accessKeyId = "LTAI5tR9TMV6z68azwfaN8iw";
-        String accessKeySecret = "0FzR9JxVf9ZKPfk8nbW9mg2AoXZhxI";
+        String accessKeyId = "*****";
+        String accessKeySecret = "****";
 // 填写Bucket名称，例如examplebucket。
-        String bucketName = "curiophil-srb-file";
+        String bucketName = "****";
 // 填写文件名。文件名包含路径，不包含Bucket名称。例如exampledir/exampleobject.txt。
         String objectName = "摘录.md";
 
